28a4c9a4aa8478b829a012030707ed8b3d605edc
the-amk <abdulla@alkhalifa.com>
2014-10-22 13:10:05 +0100

    Added better movement.

--- myrobots/SkasRobo.java
+++ myrobots/SkasRobo.java
@@ -12,6 +12,10 @@
  import robocode.*;
  import static robocode.util.Utils.normalRelativeAngleDegrees;
  public class SkasRobo extends AdvancedRobot {
+    private double previousEnergy = 100;
+    private float direction = 1;
+    private float turnDirection = 1;
+    private boolean justChangedDirection = false;
      abstract class State<T> {
          abstract void enter(T obj);
          abstract void execute(T obj);
@@ -36,8 +40,26 @@
 
        
      }
-     private float direction = 1;
-     private float turnDirection = 1;
+     
+     class Default extends State<AdvancedRobot>{
+
+        @Override
+        void enter(AdvancedRobot obj) {
+            
+        }
+
+        @Override
+        void execute(AdvancedRobot obj) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        void exit(AdvancedRobot obj) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+       
+     }
      public void run() {
         this.setAdjustGunForRobotTurn(true);
 
@@ -57,37 +79,35 @@
             
             System.out.println("Enemy ABS bearing: "+ absoluteBearing);
             double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
-
-            // If it's close enough, fire!
+            System.out.println("Enemy Velocity: "+ e.getVelocity());
+            //Fire if close enough
             if (Math.abs(bearingFromGun) <= 3) {
                     setTurnGunRight(bearingFromGun);
-                    // We check gun heat here, because calling fire()
-                    // uses a turn, which could cause us to lose track
-                    // of the other robot.
-                    if (getGunHeat() == 0) {
-                            fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
-                    }
-            } // otherwise just set the gun to turn.
-            // Note:  This will have no effect until we call scan()
+          
+            } 
             else {
                     this.setTurnGunRight(bearingFromGun);
             }
-            // Generates another scan event if we see a robot.
-            // We only need to call this if the gun (and therefore radar)
-            // are not turning.  Otherwise, scan is called automatically.
             
+            //Move to state
+            if (getGunHeat() == 0) 
+            {
+                double difference_heading = Math.abs(e.getHeading() - getHeading());
+                fire(Math.min(3, getEnergy() - .1));
+                
+            }
           
             
             
-            if(e.getDistance() < 100)
+            if(e.getDistance() < 10)
             {
                 if(e.getBearing() > 5)
                 {
-                   this.setTurnLeft(5);
+                   this.setTurnRight(3*this.direction);
                 }
                 else if(e.getBearing() < -5)
                 {
-                   this.setTurnRight(5);
+                   this.setTurnLeft(3*this.direction);
                 }
             }
             else
@@ -105,11 +125,17 @@
             
             //If the bearing is too alrge then change direction youre heading.
             
+            //Check to see if opponents energy levels have changed, this means
+            //the opponent has fired
+            double changeInEnergy =   previousEnergy-e.getEnergy();
+//         
+            previousEnergy = e.getEnergy();
+            
             this.setAhead(direction*(e.getDistance()+5.0));
             
             waitFor(new TurnCompleteCondition(this));
            
-           
+            this.justChangedDirection = false;
             scan();
             
             
@@ -132,8 +158,16 @@
     
     public void onHitWall(HitWallEvent event) {
         this.direction *= -1;
+        this.justChangedDirection = true;
         this.scan();
     }
+    
+
+    @Override
+    public void onWin(WinEvent event) {
+        super.onWin(event); //To change body of generated methods, choose Tools | Templates.
+    }
+
 
  }
 
